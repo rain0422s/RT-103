f8b3f8638d43ecf9dfb070db50b8cf2e93ee6bfb
fushuntong <shuntong_fu@techvision.com.cn>
2024-08-30 17:01:01 +0800

    [AT24CXX]:Modified files related to EEPROM at24c02
    [Description]
    Refactored function variable names to improve code readability

--- Lib/m24cxx/at24cxx.c
+++ Lib/m24cxx/at24cxx.c
@@ -10,94 +10,92 @@
 
 bool at24cxx_read(struct i2c_cli at24cxx ,uint16_t addr, uint8_t* buff, uint16_t len){
         delay_ms(5);
-        return soft_i2c_read_mem(at24cxx.bus, at24cxx.dev,addr, 
-                                        buff, len, at24cxx.ops);    
+        return soft_i2c_read_mem(
+                at24cxx.bus, at24cxx.dev,addr,buff, len, at24cxx.ops);    
 }
 
 bool at24cxx_internal_write(struct i2c_cli at24cxx ,uint16_t addr, uint8_t* buff, uint16_t len){
         delay_ms(5);
-        return soft_i2c_write_mem(at24cxx.bus, at24cxx.dev,addr, 
-                                        buff, len, at24cxx.ops);
+        return soft_i2c_write_mem(
+                at24cxx.bus, at24cxx.dev,addr,buff, len, at24cxx.ops);
 }
 
-bool at24cxx_write(struct i2c_cli at24cxx ,uint16_t addr, uint8_t* buff, uint16_t len)
-{
+bool at24cxx_write(struct i2c_cli at24cxx ,uint16_t addr, uint8_t* buff, uint16_t len){
 
-    if (addr + len > AT24CXX_MAX_ADDR) return false;
+        if(addr+len>AT24CXX_MAX_ADDR) 
+                return false;
 
-    // front
+        // front
+        uint8_t cnt = AT24CXX_PAGE_SIZE-addr%8;
+        if(cnt > 0){
+                if(len<cnt) 
+                        cnt = len;
 
-    uint8_t cnt = AT24CXX_PAGE_SIZE - addr % 8;
+                if(!at24cxx_internal_write(at24cxx,addr,buff,cnt))
+                        return false;
 
-    if (cnt > 0) {
-        if (len < cnt) cnt = len;
+                len -= cnt;
 
-        if (!at24cxx_internal_write(at24cxx ,addr, buff, cnt))
-            return false;
-
-        len -= cnt;
-        if (len == 0) return true;
-        addr += cnt;
-        buff += cnt;
-    }
-
-    // middle
-
-    while (len >= AT24CXX_PAGE_SIZE) {
-        if (at24cxx_internal_write(at24cxx ,addr, buff, AT24CXX_PAGE_SIZE)) {
-            addr += AT24CXX_PAGE_SIZE;
-            buff += AT24CXX_PAGE_SIZE;
-            len -= AT24CXX_PAGE_SIZE;
-        } else {
-            return false;
+                if(!len) 
+                        return true;
+                addr += cnt;
+                buff += cnt;
         }
-    }
 
-    // back
+        // middle
+        while(len >= AT24CXX_PAGE_SIZE){
+                if (!at24cxx_internal_write(at24cxx ,addr, buff, AT24CXX_PAGE_SIZE))
+                        return false;
 
-    if (len > 0) return at24cxx_internal_write(at24cxx ,addr, buff, len);
+                addr += AT24CXX_PAGE_SIZE;
+                buff += AT24CXX_PAGE_SIZE;
+                len  -= AT24CXX_PAGE_SIZE;
+        }
 
-    return true;
+        // back
+        if (len > 0) 
+                return at24cxx_internal_write(at24cxx ,addr, buff, len);
+
+        return true;
 }
 
 /**
  * @param wrap_num number of displays per line, range: [1,64]
  */
-bool at24cxx_print(struct i2c_cli at24cxx ,uint16_t start, uint16_t len, uint8_t wrap_num)
-{
-    if (wrap_num == 0)
-        return false;
+bool at24cxx_print(struct i2c_cli at24cxx ,uint16_t start, uint16_t len, uint8_t wrap_num){
+        uint8_t buff[64];
 
-    uint8_t buff[64];
+        if(!wrap_num)
+                return false;
 
-    if (wrap_num > 64)
-        wrap_num = 64;
+        if(wrap_num>64)
+                wrap_num = 64;
 
-    uint16_t end = start + len;
 
-    while (start < end) {
-        // read data to buff
-        if (!at24cxx_read(at24cxx ,start, buff, wrap_num)) {
-            println("fail to read at24cxx [%05d]", start);
-            return false;
+        uint16_t end = start + len;
+        while(start<end){
+                // read data to buff
+                if(!at24cxx_read(at24cxx,start,buff,wrap_num)){
+                        println("fail to read at24cxx [%05d]", start);
+                        return false;
+                }
+
+                // print pos and len
+                printf("[%05d,%05d,%02d] ", start, start + wrap_num, wrap_num);
+
+                // set next reading pos
+                start += wrap_num;
+
+                // print buffer
+                for(uint8_t j = 0; j < wrap_num; ++j)
+                        printf("%5d ", buff[j]);
+                println("");
+
+                // count of rest
+                if(start + wrap_num > end)
+                        wrap_num = end - start;
         }
 
-        // print pos and len
-        printf("[%05d,%05d,%02d] ", start, start + wrap_num, wrap_num);
-
-        // set next reading pos
-        start += wrap_num;
-
-        // print buffer
-        for (uint8_t j = 0; j < wrap_num; ++j)
-            printf("%5d ", buff[j]);
-        println("");
-
-        // count of rest
-        if (start + wrap_num > end)
-            wrap_num = end - start;
-    }
-
-    println(SEPARATOR60);
-    return true;
+        println(SEPARATOR60);
+        return true;
 }
