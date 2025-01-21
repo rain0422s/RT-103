f7afb76b5db10cf6161b40ab3cd28db3fafe1094
Rain0422 <2785204187@qq.com>
2025-01-21 09:46:42 +0800

    [Sensor]:Add read_data_simple.c

--- Lib/lis2dh12/read_data_simple.c
+++ Lib/lis2dh12/read_data_simple.c
@@ -35,25 +35,7 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
-#include "lis2dh12_reg.h"
-#include <string.h>
-#include <stdio.h>
-#define MKI109V2
-// #define NUCLEO_STM32F411RE
-
-#ifdef MKI109V2
-#include "stm32f1xx_hal.h"
-// #include "usbd_cdc_if.h"
-#include "spi.h"
-#include "i2c.h"
-#endif
-
-#ifdef NUCLEO_STM32F411RE
-#include "stm32f4xx_hal.h"
-#include "i2c.h"
-#include "usart.h"
-#include "gpio.h"
-#endif
+#include "read_data_simple.h"
 
 /* Private macro -------------------------------------------------------------*/
 #ifdef MKI109V2
---
+++ Lib/lis2dh12/read_data_simple.h
@@ -0,0 +1,28 @@
+#ifndef __IMU_DEV_H__
+#define __IMU_DEV_H__
+
+#include "lis2dh12_reg.h"
+#include <string.h>
+#include <stdio.h>
+#define MKI109V2
+
+// #define NUCLEO_STM32F411RE
+
+#ifdef MKI109V2
+#include "stm32f1xx_hal.h"
+// #include "usbd_cdc_if.h"
+#include "spi.h"
+#include "i2c.h"
+#endif
+
+#ifdef NUCLEO_STM32F411RE
+#include "stm32f4xx_hal.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+#endif
+
+
+void example_main(void);
+#endif
+
