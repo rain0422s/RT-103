29507175ca309a531d3dfdbfe5ff607be80ebee2
Rain0422 <2785204187@qq.com>
2025-03-07 11:51:15 +0800

    [GPIO]:Example Disable the external interrupt function of PA8

--- Core/Inc/stm32f1xx_it.h
+++ Core/Inc/stm32f1xx_it.h
@@ -59,7 +59,6 @@ void RTC_IRQHandler(void);
 void RCC_IRQHandler(void);
 void DMA1_Channel1_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
-void EXTI9_5_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
