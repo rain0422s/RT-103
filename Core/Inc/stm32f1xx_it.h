fdab5e6599962988a10f09084eb0751be517033d
Rain0422 <2785204187@qq.com>
2025-08-11 15:19:04 +0800

    Configure UART2 DMA interrupt

--- Core/Inc/stm32f1xx_it.h
+++ Core/Inc/stm32f1xx_it.h
@@ -57,6 +57,8 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void RCC_IRQHandler(void);
 void DMA1_Channel1_IRQHandler(void);
+void DMA1_Channel6_IRQHandler(void);
+void DMA1_Channel7_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
